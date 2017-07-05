bb9f90407701d9370a49510a4c82e3a384edec5b
leon in 't Veld <leon3110l@gmail.com>
2017-07-05 15:09:28 +0200

    fixed the _parsePathParam function

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ class TMDB {
     this.baseUrl = "api.themoviedb.org";
     this.basePath = "/3";
     this.endPath = "?api_key="+this.apiKey;
-    this.__setup();
+    this._setup();
   }
 
   // generates all the methods
-  __setup() {
+  _setup() {
     const endPoints = require("./endPoints.json");
     for (var parent in endPoints) {
       this[parent] = {};
       for (var child in endPoints[parent]) {
         this[parent][child] = (options = {}, param = {}) => {
-          console.log(this.__pathParam(endPoints[parent][child].path, param));
-          return this.__get(endPoints[parent][child].path, options);
+          console.log(this._pathParam(endPoints[parent][child].path, param));
+          return this._get(endPoints[parent][child].path, options);
         };
       }
     }
@@ -25,36 +25,37 @@ class TMDB {
 
   // options can look like this
   // {page: 2}
-  __genOptions(options) {
+  _genOptions(options) {
     let res = "";
     for (var opt in options) {
       res += "&"+opt+"="+options[opt];
     }
     return res;
   }
-  __parsePathParam(endPointPath, param) {
-    const reg = /{([^>]*)}/g; // get the stuff between the {}
-    const res = reg.exec(endPointPath);
-    console.log(res);
-    if (!res) {
-      return endPointPath;
-    }
-    let replaceWith = param[res[1]];
-    if (!param || !replaceWith) {
-      replaceWith = "";
-    }
-    endPointPath = endPointPath.replace(reg, replaceWith);
-    endPointPath = endPointPath.replace(/\/\/+/g, "/"); // remove double slashes
 
-    return endPointPath;
+  _parsePathParam(path, param){
+    const reg = /{(.*?)}/g; // get the stuff between the {}
+    const arr = path.match(reg); // array with the results of the regexp
+
+    for (var i = 0; i < arr.length; i++) {
+      let par = param[arr[i].substr(1, arr[i].length-2)];
+      if (par) {
+        path = path.replace(arr[i], par);
+      } else {
+        path = path.replace(arr[i], "");
+      }
+    }
+
+    path = path.replace(/\/\/+/g, "/"); // remove double slashes
+    return path;
   }
 
-  __request(path, options, method) {
+  _request(path, options, method) {
     return new Promise((resolve, reject) => {
       const httpsOptions = {
         hostname: this.baseUrl,
         port: 443,
-        path: this.basePath + path + this.endPath + this.__genOptions(options),
+        path: this.basePath + path + this.endPath + this._genOptions(options),
         method: method
       };
 
@@ -74,11 +75,11 @@ class TMDB {
       req.end();
     });
   }
-  __get(path, options = {}) {
-    return this.__request(path, options, "GET");
+  _get(path, options = {}) {
+    return this._request(path, options, "GET");
   }
-  __post(path, options = {}) {
-    return this.__request(path, options, "POST");
+  _post(path, options = {}) {
+    return this._request(path, options, "POST");
   }
 }
 
@@ -89,6 +90,6 @@ const tmdb = new TMDB("81485988d49a76332eea5e3a5297d342");
 //   console.log(e);
 // });
 
-console.log(tmdb.__parsePathParam("/movie/{movie_id}/videos/{dinges}", {movie_id: 297762, dinges: "bla"}));
+console.log(tmdb._parsePathParam("/movie/{movie_id}/videos/{dinges}", {movie_id: 297762, dinges: "bla"}));
 
 module.exports = TMDB;
