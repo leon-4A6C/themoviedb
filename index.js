b82188a9c11cc962d08b8e670f50bf7df9086f7b
leon in 't Veld <leon3110l@gmail.com>
2017-07-05 21:31:44 +0200

    forgot to rename a file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const EasyApi = require("easy-api-wrapper");
 // creates an Tmdb class
 class Tmdb extends EasyApi {
   constructor(apiKey) {
-    super({apiKey: apiKey, baseUrl: "api.themoviedb.org", basePath: "/3", endPoints: require("./example-endPoints.json")});
+    super({apiKey: apiKey, baseUrl: "api.themoviedb.org", basePath: "/3", endPoints: require("./endPoints.json")});
   }
 }
 
